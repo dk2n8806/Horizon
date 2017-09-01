41d1b7e4820660693027c4c7d1c8adb18051e828
dk2n8806 <dknguyen8806@gmail.com>
2017-09-01 09:33:07 -0700

    Add new properties to base entity

--- src/main/java/com/horizon/entity/AbstractBaseEntity.java
+++ src/main/java/com/horizon/entity/AbstractBaseEntity.java
@@ -1,11 +1,14 @@
 package com.horizon.entity;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.Version;
 
 @MappedSuperclass
 public abstract class AbstractBaseEntity implements Serializable
@@ -14,9 +17,28 @@ public abstract class AbstractBaseEntity implements Serializable
 	private static final long serialVersionUID = 1L;
 
 	private long id;
+	private long version;
+	private LocalDateTime createdOn;
+	private LocalDateTime modifiedOn;
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	public long getId() {return id;}
 	public void setId(long id) {	this.id = id;}
+	
+	@Version
+	@Column(name="VERSION")
+	public long getVersion() {	return version;}
+	public void setVersion(long version) {	this.version = version;}
+	
+	
+	@Column(name="CREATED_ON")
+	public LocalDateTime getCreatedOn() {	return createdOn;}
+	public void setCreatedOn(LocalDateTime createdOn) {	this.createdOn = createdOn;}
+	
+	
+	@Column(name="MODIFIED_ON")
+	public LocalDateTime getModifiedOn() {	return modifiedOn;}
+	public void setModifiedOn(LocalDateTime modifiedOn) {	this.modifiedOn = modifiedOn;}
+	
 }
