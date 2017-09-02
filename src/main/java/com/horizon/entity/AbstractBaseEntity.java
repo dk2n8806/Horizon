93a096d8efae522778aa74b816b6b6c6f21b0ae6
dk2n8806 <dknguyen8806@gmail.com>
2017-09-02 14:26:46 -0700

    Update Id type

--- src/main/java/com/horizon/entity/AbstractBaseEntity.java
+++ src/main/java/com/horizon/entity/AbstractBaseEntity.java
@@ -2,13 +2,17 @@ package com.horizon.entity;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.Version;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 @MappedSuperclass
 public abstract class AbstractBaseEntity implements Serializable
@@ -16,28 +20,31 @@ public abstract class AbstractBaseEntity implements Serializable
 
 	private static final long serialVersionUID = 1L;
 
-	private long id;
+	private UUID id;
 	private long version;
 	private LocalDateTime createdOn;
 	private LocalDateTime modifiedOn;
 	
+	
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	public long getId() {return id;}
-	public void setId(long id) {	this.id = id;}
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)")
+	public UUID getId() {return id;}
+	public void setId(UUID id) {	this.id = id;}
 	
 	@Version
 	@Column(name="VERSION")
 	public long getVersion() {	return version;}
 	public void setVersion(long version) {	this.version = version;}
 	
-	
+	@CreatedDate
 	@Column(name="CREATED_ON", nullable=false, updatable=false)
 	public LocalDateTime getCreatedOn() {	return createdOn;}
 	public void setCreatedOn(LocalDateTime createdOn) {	this.createdOn = createdOn;}
 	
-	
-	@Column(name="MODIFIED_ON")
+	@LastModifiedDate
+	@Column(name="MODIFIED_ON", nullable=false)
 	public LocalDateTime getModifiedOn() {	return modifiedOn;}
 	public void setModifiedOn(LocalDateTime modifiedOn) {	this.modifiedOn = modifiedOn;}
 	
